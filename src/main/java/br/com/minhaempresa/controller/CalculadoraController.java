e79f0bc482eff1064e4905ee4ad507b1907fb30d
erictomson <erictomson@gmail.com>
2021-10-06 18:39:01 -0300

    Corrigidos métodos de cálculo

--- src/main/java/br/com/minhaempresa/controller/CalculadoraController.java
+++ src/main/java/br/com/minhaempresa/controller/CalculadoraController.java
@@ -1,5 +1,8 @@
 package br.com.minhaempresa.controller;
 
+import br.com.minhaempresa.domain.Operacao;
+import br.com.minhaempresa.service.CalculadoraService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,11 +10,44 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/calculadora")
+@WebServlet(urlPatterns = "/calcular")
 public class CalculadoraController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        double operandoA=0;
+        double operandoB=0;
+        int operador=0;
+        try {
+            // Atribuindo valores através dos parâmetros de requisição
+            operandoA= Double.valueOf(req.getParameter("operandoA"));
+            operandoB = Double.valueOf(req.getParameter("operandoB"));
+            operador = Integer.valueOf(req.getParameter("operador"));
+        } catch (NumberFormatException e) {
+            resp.getWriter().println("Entrada inválida! Digite um número inteiro.");
+        }
+
+        double resultado = 0;
+
+        CalculadoraService calculadoraService = new CalculadoraService();
+
+        Operacao operacao = null;
+
+        switch(operador) {
+            case 0: {operacao = Operacao.SOMA;break;}
+            case 1: {operacao = Operacao.SUBTRACAO;break;}
+            case 2: {operacao = Operacao.MULTIPLICACAO;break;}
+            case 3: {operacao = Operacao.DIVISAO;break;}
+            default:{resp.getWriter().println("Operacao inválida");}
+        }
+
+        resultado = calculadoraService.calcular(operandoA,operandoB,operacao);
+
+        resp.getWriter().println("Resultado: " + resultado);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Servlet no ar");
     }
 }
--- src/main/java/br/com/minhaempresa/service/CalculadoraService.java
+++ src/main/java/br/com/minhaempresa/service/CalculadoraService.java
@@ -1,35 +1,50 @@
 package br.com.minhaempresa.service;
 
+import br.com.minhaempresa.domain.Operacao;
+
 public class CalculadoraService {
 
-    public double calcular(double operandoA, double operandoB, int operacao) {
+    public double calcular(double operandoA, double operandoB, Operacao operacao) {
 
         double resultado=0;
 
-        switch (operacao) {
-            case 0 -> resultado = somar(operandoA,operandoB);
-            case 1 -> resultado = subtrair(operandoA,operandoB);
-            case 2 -> resultado = multiplicar(operandoA,operandoB);
-            case 3 -> resultado = dividir(operandoA,operandoB);
-            default -> {}
+        switch(operacao) {
+            case SOMA: {
+                resultado = this.somar(operandoA, operandoB);
+                break;
+            }
+            case SUBTRACAO: {
+                resultado = this.subtrair(operandoA, operandoB);
+                break;
+            }
+            case MULTIPLICACAO: {
+                resultado = this.multiplicar(operandoA, operandoB);
+                break;
+            }
+            case DIVISAO: {
+                resultado = this.dividir(operandoA, operandoB);
+                break;
+            }
         }
-
         return resultado;
     }
 
     private double somar(double operandoA, double operandoB) {
-        return operandoA + operandoB;
+        return (operandoA + operandoB);
     }
 
     private double subtrair(double operandoA, double operandoB) {
-        return operandoA - operandoB;
+        return (operandoA - operandoB);
     }
 
     private double multiplicar(double operandoA, double operandoB) {
-        return operandoA * operandoB;
+        return (operandoA * operandoB);
     }
 
     private double dividir(double operandoA, double operandoB) {
-        return operandoA / operandoB;
+        if(operandoB==0) {
+            throw new ArithmeticException("O operando B deve ser diferente de zero!");
+        }
+        return (operandoA / operandoB);
     }
 }
